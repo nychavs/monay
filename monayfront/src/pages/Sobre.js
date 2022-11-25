296b5d8a5b8448335fd327726d32a755d49acd22
nychavs <chavesnaiury@gmail.com>
2022-11-25 11:25:17 -0300

    chaged sobre

--- monayfront/src/pages/Sobre.js
+++ monayfront/src/pages/Sobre.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import '../styles/Sobre.css'
 import '../scripts';
-
+import Doubts from '../components/Doubts';
+import Card from '../components/Card';
+import foto from '../assets/3.png'
 function Sobre() {
   return (
-    <div>Sobre
-      <h1>teste</h1>
+    <div className='sobre-container'>
+      <div className='sobre-header' 
+      style={{ backgroundImage: `url(${foto})`}}>
+        <p>Somos o next. Um banco 100% digital e 
+          gratuito, que nasceu com a missão de oferecer 
+          para todos os brasileiros a oportunidade de 
+          ter uma conta e um cartão sem custos, taxas ou 
+          anuidade. Acreditamos que desta maneira mais 
+          pessoas terão acesso a uma vida financeira 
+          organizada e a oportunidade de realizar seus sonhos.</p>
+      </div>
       <div id='posts-container'>
-        <p>a</p>
+        <Doubts></Doubts>
+        <Card></Card>
       </div>
     </div>
   )
